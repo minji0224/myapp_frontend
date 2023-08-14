89bbf8e7a3ba8b9cb36988856c797f8cf34ffb81
Minji Choi <minj22222c@gmail.com>
2023-08-14 17:16:32 +0900

    메인페이지 진행중

---
+++ post/index.js
@@ -0,0 +1,12 @@
+let currentPage = 0;
+let isLastPage = false;
+const PAGE_SIZE = 8;
+let currentQuery = "";
+
+function cardTemplate(i) { /*html*/
+  const template = <div class= "template" data-no="${i.no}">
+  
+
+  </div>
+
+}
