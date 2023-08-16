e726a0df82e0547e825fd52c938a7f7b2599fc1c
Minji Choi <minj22222c@gmail.com>
2023-08-16 11:46:18 +0900

    첫페이지 및 페이징 처리 진행 중

--- post/index.js
+++ post/index.js
@@ -3,52 +3,60 @@ let isLastPage = false;
 const PAGE_SIZE = 8;
 let currentQuery = "";
 
-
-// 카드템플릿 매개변수 수정하고 밑에도 수정해야함
-
-function cardTemplate(i) { /*html*/
-
- const time = new Date(i.createdTime);
+// 게시물 생성 함수
+function cardTemplate(item) { /*html*/
+ const time = new Date(item.createdTime);
  const formattedTime = `${time.getFullYear()}년
- ${(time.getMonth() + 1).toString()}월
- ${time.getDate().toString()}일
- ${time.getHours().toString()}시
- ${time.getMinutes().toString()}분
- ${time.getSeconds().toString()}초`;
+  ${(time.getMonth() + 1).toString()}월
+  ${time.getDate().toString()}일
+  ${time.getHours().toString()}시
+  ${time.getMinutes().toString()}분
+  ${time.getSeconds().toString()}초`;
 
-
-  const template = `<div class= "template" data-no="${i.no}">
-  <button name="btn-remove"></button>
-  <span>${i.no} ${i.creatorName}</span>
-  <p>${i.link}</p>
-  <h3>${i.restaurantName}</h3>
-  <p>${i.content}</p>
+ const template = `<div class= "template" data-no="${item.no}">
+  <button name="btn-remove">X</button>
+  <span>${item.no} ${item.creatorName}</span>
+  <p>${item.link}</p>
+  <h3>${item.restaurantName}</h3>
+  <p>${item.content}</p>
   <span>${formattedTime}</span>
   </div>`;
 
   return template;
 }
 
+
+// 페이징 처리
 async function getPagedList(page, query) {
   let url = "";
   if(query) {
-    url = `http://localhost:8080/contacts/paging/search?page=${page}&size=${PAGE_SIZE}&query=${query}`;
+    url = `http://localhost:8080/posts/paging/search?page=${page}&size=${PAGE_SIZE}&query=${query}`;
   } else {
-    url = `http://localhost:8080/contacts/paging?page=${page}&size=${PAGE_SIZE}`;
+    url = `http://localhost:8080/posts/paging?page=${page}&size=${PAGE_SIZE}`;
   }
 
-  const response = await fetch(url, {
-    method: "POST"
-  });
-
+  const response = await fetch(url);
   const result = await response.json();
-  document.querySelectorAll("form")[0].insertAdjacentHTML = "";
+
+  const container = document.querySelector("#container");
+
+  container.innerHTML = "";
+
   for(let item of result.content){
-    document.querySelectorAll("form")[0].insertAdjacentHTML("afterbegin", template());
+    container.insertAdjacentHTML("afterbegin",cardTemplate(item));
   }
+
   currentPage = result.number;
   isLastPage = result.last; 
 
-  setBtnActive();
+  // setBtnActive();
   
 }
+
+// 첫화면(첫번째페이지)
+(async() => {
+  window.addEventListener("DOMContentLoaded", () => {
+    getPagedList(0);
+  });
+})();
+
